a82bf095b2d5a6290ef3468d096308a71e811b6a
Lana-chan <erin.pinheiro@usp.br>
2016-05-11 19:28:57 -0300

    refatoração, busca ano inteiro

--- client/www/js/agenda.js
+++ client/www/js/agenda.js
@@ -1,12 +1,16 @@
 SERVER = "http://labhacker.org.br:5000/api/evento";
-var cacheCalendario;
-var cacheEventos;
+var JSONcache = [];
 var dataSelecionada;
-var datasComEventos;
+// matriz[ano][mes][dia] = num eventos
+var matrizEventos = new Object();
 
 $(document).on("pageinit","#index",function(){ // When entering index
-  carregaHoje();
+  // mostraEventos usa dataSelecionada
+  // carrega eventos desse mês quando boot
+  dataSelecionada = new Date();
+  carregaAno(null, mostraEventos);
   
+  // um swipe right no index abre menu
   $(document).on("swiperight", "#index", openMenu);
   
   var datept = {
@@ -26,17 +30,19 @@ $(document).on("pageinit","#index",function(){ // When entering index
 });
 
 function onLoad() {
+  // hook do cordova quando dispostivo inicializou
   document.addEventListener("deviceready", onDeviceReady, false);
 }
 
 function onDeviceReady() {
+  // inicializa botões hardware do dispositivo
   document.addEventListener("backbutton", closeMenus, false);
   navigator.app.overrideButton("menubutton", true);
   document.addEventListener("menubutton", openMenu, false);
 }
 
 function closeMenus() {
-  if($.mobile.activePage.jqmData("popup") == "open") { // primeiro fecha popups, não funciona
+  if($.mobile.activePage.jqmData("popup") == "open") { // primeiro fecha popups, TODO: não funciona
     $("[data-role=popup]").popup("close");
   } else if($.mobile.activePage.jqmData("panel") == "open") { // segundo fecha paineis
     $("[data-role=panel]").panel("close");
@@ -47,29 +53,27 @@ function closeMenus() {
 
 function openMenu() {
   if($.mobile.activePage.jqmData("panel") !== "open") {
-    //if(e.type === "swipeleft") {
-      $("#menu").panel("open");
-    //}
+    $("#menu").panel("open");
   }
 }
 
-function carregaMes(date) {
+// carrega um ano inteiro do DB para o cache JSON
+// date = ano, callback = função a rodar quando sucesso
+function carregaAno(date, callback) {
   if(date == null) {
-    date = new Date();
+    date = new Date().getFullYear();
   }
-  var startDate = new Date(date.getFullYear(), date.getMonth(), 1);
-  var endDate = new Date(date.getFullYear(), date.getMonth()+1, 0); //hack !!!!
-  var formattedStartDate = $.datepicker.formatDate('yy-mm-dd', startDate);
-  var formattedEndDate = $.datepicker.formatDate('yy-mm-dd', endDate);
+  var startDate = date + '-01-01';
+  var endDate = date + '-12-31';
   var query = { 
     "filters": [{
       "name":"data_inicio",
       "op":"gte",
-      "val": formattedStartDate
+      "val": startDate
     },{
       "name":"data_inicio",
       "op":"lte",
-      "val": formattedEndDate
+      "val": endDate
     }],
     "order_by": [{
       "field":"data_inicio",
@@ -83,27 +87,36 @@ function carregaMes(date) {
   $.ajax({
     url: url,
     dataType: "json",
-    async: false,
+    async: true,
     beforeSend: function() {
-      // This callback function will trigger before data is sent
-      $.mobile.loading('show', {theme:"a", text:"Aguarde...", textonly:false, textVisible: true}); // This will show ajax spinner
+      setTimeout(function() {
+        $.mobile.loading('show', {theme:"a", text:"Aguarde...", textonly:false, textVisible: true}); // This will show ajax spinner
+      }, 1);
     },
     complete: function() {
       // This callback function will trigger on data sent/received complete
-      $.mobile.loading('hide'); // This will hide ajax spinner
+      setTimeout(function() {
+        $.mobile.loading('hide'); // This will hide ajax spinner
+      }, 1);
     },
     success: function (result) {
       //parseJSON(result);
-      cacheCalendario = result;
-      // pega datas, põe num array para markDates
-      datasComEventos = [];
+      // matriz de dias carregados
       for(var v in result.objects) {
         var obj = result.objects[v];
-        var adate = $.datepicker.formatDate('yy-mm-dd', new Date(obj.data_inicio));
-        if($.inArray(adate, datasComEventos) == -1) { //se não está no array
-          datasComEventos[datasComEventos.length] = adate;
-        }
+        JSONcache.push(obj);
+        var adate = new Date(obj.data_inicio);
+        var eventYear = adate.getFullYear();
+        var eventMonth = adate.getMonth()+1;
+        if(matrizEventos[eventYear] == null) matrizEventos[eventYear] = new Object();
+        if(matrizEventos[eventYear][eventMonth] == null) matrizEventos[eventYear][eventMonth] = new Array();
+        matrizEventos[adate.getFullYear()][adate.getMonth()+1].push(adate.getDate());
+        console.log(matrizEventos[eventYear]);
       }
+      
+      $("#data").date("refresh");
+      
+      callback();
     },
     error: function (request,error) {
       alert('Não foi possível acessar o servidor!');
@@ -113,37 +126,60 @@ function carregaMes(date) {
   return true;
 }
 
+// chamado quando clica busca no mês
 function carregaBusca() {
   $("[data-role=panel]").panel("close");
   $("[data-role=popup]").popup("close");
   //carregaEventos($('#data').date('getDate'));
+  console.log(dataSelecionada);
   mostraEventos();
 }
 
-function carregaHoje() {
-  if(carregaMes()) {
-    mostraEventos();
-  }
-  // se não der isso, deu erro!!
-}
-
+// retorna data x dias no futuro
 function addDays(date, days) {
     var result = new Date(date);
     result.setDate(date.getDate() + days);
     return result;
 }
 
+function filtraEventos(startDate, endDate) {
+  var out = [];
+  for(v in JSONcache) {
+    var obj = JSONcache[v];
+    var eventDate = new Date(obj.data_inicio);
+    console.log(eventDate);
+    if(eventDate >= startDate && eventDate <= endDate) {
+      out.push(obj);
+    }
+  }
+  return out;
+}
+
+// chamado para atualizar eventos no index
 function mostraEventos() {
-  cacheEventos = cacheCalendario;
-  var result = cacheEventos; // porco? talvez
+  // result precisa conter apenas eventos a serem mostrados (filtrar mês selecionado)
+  // JSONcache deve conter todos os eventos baixados (geralmente, todo o ano atual)
+  
+  var startDate = new Date(dataSelecionada.getFullYear(), dataSelecionada.getMonth(), 1);
+  var endDate = new Date(dataSelecionada.getFullYear(), dataSelecionada.getMonth()+1, 0);
+  
+  console.log(startDate);
+  console.log(endDate);
+  
+  console.log(JSONcache);
+  
+  var result = filtraEventos(startDate, endDate);
+  
+  console.log(result);
+
   var content = "";
-  if(result.num_results < 1) {
+  if(result.length < 1) {
     content += "<p>Não há eventos para a data escolhida!</p>";
   } else {
     content += '<ul data-role="listview">';
     var date = new Date(0);
-    for(var v in result.objects) {
-      var obj = result.objects[v];
+    for(var v in result) {
+      var obj = result[v];
       var newdate = new Date(obj.data_inicio);
       if(newdate > addDays(date, 1)) {
         date = newdate;
@@ -163,10 +199,11 @@ function mostraEventos() {
   $("#eventos").trigger("create");
 }
 
+// chamado para abrir painel com informações do evento selecionado
 function loadEvent(id) {
   var content = "";
-  for(var v in cacheEventos.objects) {
-    var obj = cacheEventos.objects[v];
+  for(var v in JSONcache.objects) {
+    var obj = JSONcache.objects[v];
     if(obj.id == id) {
       content += "<h2>" + obj.titulo + "</h2>";
       content += "<h4>" + obj.descricao + "</h4>";
@@ -186,17 +223,18 @@ function loadEvent(id) {
   alert("Não foi possível encontrar o evento selecionado");
 }
 
+// chamado quando usuário muda mês no calendário
 function updateMonth(year, month, inst) {
-  dataSelecionada = new Date(year, month-1, 1); // ????? que há com o mês? não sei
-  if(!carregaMes(dataSelecionada)) {
-    //erro!!!
-  }
+  // TODO: checa se ano está carregado
+  dataSelecionada = new Date(year, month-1, 01);
 }
 
+// chamado a cada dia do calendário para marcar os que têm eventos
 function markDates(date) {
-  var m = $.datepicker.formatDate('yy-mm-dd', date);
-  if($.inArray(m, datasComEventos) > -1) { //se está no array
-    return [false, 'ui-state-highlight'];
+  if(matrizEventos[date.getFullYear()] != null && matrizEventos[date.getFullYear()][date.getMonth()+1] != null) {
+    if(matrizEventos[date.getFullYear()][date.getMonth()+1].indexOf(date.getDate()) > -1) { //se há evento no dia
+      return [false, 'ui-state-highlight'];
+    }
   }
   return false;
 }
