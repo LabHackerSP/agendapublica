3817acf5635b1ad455bd2969fbb5eeb60d5413e6
Pedro Markun <pedro@markun.com.br>
2016-09-13 19:19:52 -0300

    modificacoes no admin

--- djangoserver/agendapublica/admin.py
+++ djangoserver/agendapublica/admin.py
@@ -2,7 +2,24 @@ from django.contrib import admin
 
 from agendapublica.models import Evento, Orgao, Responsavel, Tipo, Tag
 
-admin.site.register(Evento)
+from django.forms import ModelForm, Textarea, TextInput, RadioSelect,CheckboxSelectMultiple
+
+class EventoForm(ModelForm):
+    class Meta:
+        model = Evento
+        fields = ('titulo', 'orgao', 'responsavel', 'tipo','local',
+         'data_inicio', 'data_fim','descricao','link','cartaz','tags')
+        widgets = {
+            'titulo': TextInput(),
+            'tipo' : RadioSelect(),
+            'local' : TextInput(),
+            'tags' : CheckboxSelectMultiple()
+        }
+
+class EventoAdmin(admin.ModelAdmin):
+    form = EventoForm
+
+admin.site.register(Evento, EventoAdmin)
 admin.site.register(Orgao)
 admin.site.register(Responsavel)
 admin.site.register(Tipo)
