f80a23ec065f3a7e3a1310ea65acd50e6acc420d
Pedro Markun <pedro@markun.com.br>
2016-09-13 18:37:56 -0300

    mudancas no limit e exposicao das tags

--- djangoserver/agendapublica/api/resources.py
+++ djangoserver/agendapublica/api/resources.py
@@ -1,22 +1,29 @@
 from tastypie import fields
 from tastypie.resources import ModelResource, Resource
-from agendapublica.models import Evento
+from agendapublica.models import Evento, Tag
 
+class TagResource(ModelResource):
+    class Meta:
+        resource_name = 'tag'
+        queryset = Tag.objects.all()
+    def determine_format(self, request):
+        return 'application/json'
 
 class EventoResource(ModelResource):
-	limit = 0
-	max_limit = 0
     orgao = fields.CharField(attribute="orgao")
     tipo = fields.CharField(attribute="tipo")
+    tags = fields.ToManyField('agendapublica.api.resources.TagResource', 'tags', full=True)
 
     class Meta:
+        limit = 0
+        max_limit = 0
         resource_name = 'evento'
         queryset = Evento.objects.all()
         allowed_methods = ['get']
 
         filtering = {
-        	'data_inicio' : ['gte'],
-        	'data_fim' : ['lte']
+            'data_inicio' : ['gte'],
+            'data_fim' : ['lte']
         }
 
     def determine_format(self, request):
--- djangoserver/agendapublica/urls.py
+++ djangoserver/agendapublica/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from tastypie.api import Api
 
-from agendapublica.api.resources import EventoResource
+from agendapublica.api.resources import EventoResource, TagResource
 
-evento = EventoResource()
+api = Api(api_name='v1')
+api.register(EventoResource())
+api.register(TagResource())
 
 urlpatterns = [
-    url(r'^api/', include(evento.urls))
+    url(r'^api/', include(api.urls))
 ]
