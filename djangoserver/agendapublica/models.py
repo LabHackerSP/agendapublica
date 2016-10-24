b0bb84f0d01f2571368911f65a3e9b761db30868
Lana-chan <erin.pinheiro@usp.br>
2016-10-24 17:03:01 -0200

    compatibilidade python3

--- djangoserver/agendapublica/models.py
+++ djangoserver/agendapublica/models.py
@@ -2,23 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
  
 class Tag(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
 class Orgao(models.Model):
-    nome = models.CharField()
+    nome = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.nome
 
 class Tipo(models.Model):
-    tipo = models.CharField()
+    tipo = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.tipo
 
 class Responsavel(models.Model):
@@ -27,22 +28,22 @@ class Responsavel(models.Model):
     department = models.CharField(max_length=100)
     orgao = models.ForeignKey(Orgao)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.user.username
 
 class Evento(models.Model):
-    titulo = models.CharField()
+    titulo = models.CharField(max_length=255)
     orgao = models.ForeignKey(Orgao, on_delete=models.CASCADE)
     responsavel = models.ForeignKey(Responsavel, on_delete=models.CASCADE)
     tipo = models.ForeignKey(Tipo)
-    local = models.CharField()
-    endereco = models.CharField()
+    local = models.TextField()
+    endereco = models.TextField()
     data_inicio = models.DateTimeField()
     data_fim = models.DateTimeField()
     descricao = models.TextField()
     link = models.URLField(blank=True)
     cartaz = models.ImageField(blank=True)
-    tags = models.ManyToManyField('Tag')
+    tags = models.ManyToManyField('Tag', blank=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.titulo
