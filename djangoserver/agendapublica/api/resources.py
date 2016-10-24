753e7a9d15e3ed09ae966d3c97fe770584198459
Lana-chan <erin.pinheiro@usp.br>
2016-10-24 19:03:54 -0200

    api tags retorna número de eventos apenas condicionalmente

--- djangoserver/agendapublica/api/resources.py
+++ djangoserver/agendapublica/api/resources.py
@@ -3,7 +3,11 @@ from tastypie.resources import ModelResource, Resource, ALL_WITH_RELATIONS
 from agendapublica.models import Evento, Tag
 
 class TagResource(ModelResource):
-    eventos = fields.ToManyField('agendapublica.api.resources.EventoResource', 'evento_set')
+    #retorna número de eventos que contém a tag quando api tag chamada com ?eventos=1
+    def dehydrate(self, bundle):
+        if bundle.request.GET.get('eventos'):
+            bundle.data['eventos'] = bundle.obj.evento_set.count()
+        return bundle
 
     class Meta:
         limit = 0
